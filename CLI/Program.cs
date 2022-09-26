596f64a7a00da8e1ca6ccb836a3add5d43206abb
izen57 <golovoslon@gmail.com>
2022-09-26 15:01:02 +0300

    gitignore update

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 using Serilog;
-using Serilog.Core;
 
 namespace CLI
 {
